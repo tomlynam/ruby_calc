211118b5691b7f068e3b287faa10a94395177108
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-12 14:11:47 -0600

    add all functionality

--- calc.rb
+++ calc.rb
@@ -1,11 +1,39 @@
-# print a welcome message
-# get first number
-# get operator
-# validate correct operator
-# get second number
 # print output
 # prompt to continue or exit
 
-def some_method
-	puts "What's your number?"
+
+def run
+	print_header
+	first_num = get_num('first')
+	op = operator
+	second_num = get_num('second')
+	puts first_num.public_send(op, second_num)
+end
+
+
+def get_num(place)
+	puts "Enter #{place} number"
+	gets.to_f
+
+
+def print_header
+	puts "Welcome to the ruby Calculator"
+	puts "______________________________"
+end
+
+
+def operator
+	valid = false
+	until valid
+		puts "Enter operator (+, -, /, *)"
+		op = gets.strip
+		valid = op =~ /\+|-|\*|\//
+	end
+	op 
+end
+
+while true
+	run
+	puts "Type q to exit or any key to continue"
+	exit if gets.strip == 'q'
 end
